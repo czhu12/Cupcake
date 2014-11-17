57bce35493a1518694deefb7b90a2ee0612ef978
Chris Zhu <chris_zhu12@hotmail.com>
2014-11-17 00:09:40 -0800

    added everything necessary

---
+++ src/Driver.java
@@ -0,0 +1,9 @@
+import annotations.Model;
+
+@Model
+public class Driver {
+	
+	public static void main(String args[]) {
+		
+	}
+}
---
+++ src/processors/ModelProcessor.java
@@ -0,0 +1,67 @@
+package processors;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.annotation.Resource;
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.TypeElement;
+
+import annotations.ColumnName;
+import annotations.Model;
+
+@SupportedAnnotationTypes({"annotations.Model"})
+public class ModelProcessor extends AbstractProcessor{
+
+	@Override
+	public boolean process(Set<? extends TypeElement> annotations,
+			RoundEnvironment roundEnv) {
+		// TODO Auto-generated method stub
+		Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(Model.class);
+		for (Element element : elements) {
+			System.out.println(element.asType());
+			if (element.getKind() == ElementKind.CLASS) {
+				List<? extends Element> innerElements = element.getEnclosedElements();
+				for (Element innerElement : innerElements) {
+					if (innerElement.getKind() == ElementKind.FIELD) {
+						ColumnName anno = innerElement.getAnnotation(ColumnName.class);
+						
+						if (anno != null) {
+							System.out.println("Successfully found one in the thing");
+						} else {
+							System.out.println("Successfully found one not in the thing");
+						}
+						/*
+						List<? extends AnnotationMirror> mirrors = innerElement.getAnnotationMirrors();
+						for (AnnotationMirror mirror : mirrors) {
+							mirror.getAnnotationType();
+						}
+						*/
+					}
+				}
+				
+				//processingEnv.getMessager().printMessage(Kind.ERROR, "New Error message");
+			}
+		}
+		return false;
+	}
+	
+	@Override
+	public SourceVersion getSupportedSourceVersion() {
+		return SourceVersion.latestSupported();
+	}
+	
+	@Override
+	public Set<String> getSupportedAnnotationTypes() {
+		Set<String> set = new HashSet<String>();
+		set.add(Model.class.getName());
+		return set;
+	}
+}
